f16f7cb120f154fd3d1e2fc41a349b1fc69145ae
Huo Chen <neversakura@hotmail.com>
2019-10-27 23:44:20 -0700

    Retry auto build codecov for master

--- test/coupling.jl
+++ test/coupling.jl
@@ -7,6 +7,13 @@ res = c(0.2)
 @test res[1] == 2π*σz⊗σi
 @test res[2] == 2π*σi⊗σz
 
+c = ConstantCouplings(["ZI", "IZ"], unit=:ħ)
+@test c.mats[1] == σz⊗σi
+@test c.mats[2] == σi⊗σz
+res = c(0.2)
+@test res[1] == σz⊗σi
+@test res[2] == σi⊗σz
+
 c = ConstantCouplings(["ZI", "IZ"], sp=true)
 @test c.mats[1] == 2π*spσz⊗spσi
 @test c.mats[2] == 2π*spσi⊗spσz
