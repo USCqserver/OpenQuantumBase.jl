ebc56339474e1d604b015ea449cb06d6b4f0099d
Chen Huo <huochen@usc.edu>
2020-04-14 00:57:11 +0000

    Fix dimensionless_time option for Redfield

--- src/opensys/redfield.jl
+++ src/opensys/redfield.jl
@@ -7,7 +7,7 @@ Defines Redfield operator
 
 $(FIELDS)
 """
-struct Redfield <: AbstractOpenSys
+struct Redfield{is_const} <: AbstractOpenSys
     """system-bath coupling operator"""
     ops
     """close system unitary"""
@@ -17,78 +17,117 @@ struct Redfield <: AbstractOpenSys
 end
 
 
-function (R::Redfield)(du, u, tf::Real, t::Real)
+Redfield(ops::ConstantCouplings, unitary, cfun) =
+    Redfield{true}(ops, unitary, cfun)
+
+Redfield(ops::AbstractTimeDependentCouplings, unitary, cfun) =
+    Redfield{false}(ops, unitary, cfun)
+
+
+function (R::Redfield{true})(du, u, tf::Real, t::Real)
     tf² = tf^2
     for S in R.ops
-        Λ, err = Λ_calculation(t, S, R.cfun, R.unitary; rtol = 1e-6, atol = 1e-8)
-        𝐊₂ = redfield_K(S, Λ, u, t)
+        #TODO Expose the error tolerance for integration
+        function integrand(x)
+            unitary = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * unitary * S * unitary'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        𝐊₂ = S * Λ * u - Λ * u * S
         𝐊₂ = 𝐊₂ + 𝐊₂'
         axpy!(-tf², 𝐊₂, du)
     end
 end
 
 
-function (R::Redfield)(du, u, tf::UnitTime, t::Real)
+(R::Redfield{true})(du, u, tf::UnitTime, t::Real) = R(du, u, 1.0, t)
+
+
+
+function (R::Redfield{false})(du, u, tf::Real, t::Real)
+    tf² = tf^2
     for S in R.ops
-        Λ, err = Λ_calculation(t, S, R.cfun, R.unitary; rtol = 1e-6, atol = 1e-8)
-        𝐊₂ = redfield_K(S, Λ, u, t)
+        function integrand(x)
+            unitary = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * unitary * S(x) * unitary'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        𝐊₂ = S(t) * Λ * u - Λ * u * S(t)
+        𝐊₂ = 𝐊₂ + 𝐊₂'
+        axpy!(-tf², 𝐊₂, du)
+    end
+end
+
+
+function (R::Redfield{false})(du, u, tf::UnitTime, t::Real)
+    for S in R.ops
+        function integrand(x)
+            unitary = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * unitary * S(x / tf) * unitary'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        𝐊₂ = S(t / tf) * Λ * u - Λ * u * S(t / tf)
         𝐊₂ = 𝐊₂ + 𝐊₂'
         axpy!(-1.0, 𝐊₂, du)
     end
 end
 
 
-@inline redfield_K(S::Matrix{T}, Λ, u, t) where {T<:Number} = S * Λ * u - Λ * u * S
-@inline redfield_K(S, Λ, u, t) = S(t) * Λ * u - Λ * u * S(t)
-
-
-function Λ_calculation(t, op::Matrix{T}, cfun, unitary; rtol = 1e-8, atol = 1e-8) where {T<:Number}
-    function integrand(x)
-        u = unitary(t) * unitary(x)'
-        cfun(t - x) * u * op * u'
-    end
-    res = quadgk(integrand, 0, t, rtol = rtol, atol = atol)
-end
-
-
-function Λ_calculation(t, op, cfun, unitary; rtol = 1e-8, atol = 1e-8)
-    function integrand(x)
-        u = unitary(t) * unitary(x)'
-        cfun(t - x) * u * op(x) * u'
-    end
-    res = quadgk(integrand, 0, t, rtol = rtol, atol = atol)
-end
-
-
-function update_vectorized_cache!(cache, R::Redfield, tf::Real, t::Real)
+function update_vectorized_cache!(cache, R::Redfield{true}, tf::Real, t::Real)
     tf² = tf^2
     iden = Matrix{eltype(cache)}(I, size(R.ops))
     for S in R.ops
-        Λ, err = Λ_calculation(t, S, R.cfun, R.unitary; rtol = 1e-6, atol = 1e-8)
-        _update_vectorized_cache!(cache, S, Λ, iden, tf², t)
+        function integrand(x)
+            unitary = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * unitary * S * unitary'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        SΛ = S * Λ
+        cache .-=
+            tf² * (iden ⊗ SΛ + conj(SΛ) ⊗ iden - transpose(S) ⊗ Λ - conj(Λ) ⊗ S)
     end
 end
 
 
-function update_vectorized_cache!(cache, R::Redfield, tf::UnitTime, t::Real)
+update_vectorized_cache!(cache, R::Redfield{true}, tf::UnitTime, t::Real) =
+    update_vectorized_cache!(cache, R, 1.0, t)
+
+
+function update_vectorized_cache!(cache, R::Redfield{false}, tf::Real, t::Real)
+    tf² = tf^2
     iden = Matrix{eltype(cache)}(I, size(R.ops))
     for S in R.ops
-        Λ, err = Λ_calculation(t, S, R.cfun, R.unitary; rtol = 1e-6, atol = 1e-8)
-        _update_vectorized_cache!(cache, S, Λ, iden, 1.0, t)
+        function integrand(x)
+            u = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * u * S(x) * u'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        Sm = S(t)
+        SΛ = Sm * Λ
+        cache .-=
+            tf² *
+            (iden ⊗ SΛ + conj(SΛ) ⊗ iden - transpose(Sm) ⊗ Λ - conj(Λ) ⊗ Sm)
     end
 end
 
 
-@inline function _update_vectorized_cache!(cache, S::Matrix{T}, Λ, iden, tf², t) where T<:Number
-    SΛ = S * Λ
-    cache .-= tf² * (iden ⊗ SΛ + conj(SΛ) ⊗ iden - transpose(S) ⊗ Λ - conj(Λ) ⊗ S)
-end
-
-
-@inline function _update_vectorized_cache!(cache, Sfun, Λ, iden, tf², t)
-    S = Sfun(t)
-    SΛ = S * Λ
-    cache .-= tf² * (iden ⊗ SΛ + conj(SΛ) ⊗ iden - transpose(S) ⊗ Λ - conj(Λ) ⊗ S)
+function update_vectorized_cache!(
+    cache,
+    R::Redfield{false},
+    tf::UnitTime,
+    t::Real,
+)
+    iden = Matrix{eltype(cache)}(I, size(R.ops))
+    for S in R.ops
+        function integrand(x)
+            u = R.unitary(t) * R.unitary(x)'
+            R.cfun(t - x) * u * S(x / tf) * u'
+        end
+        Λ, err = quadgk(integrand, 0, t, rtol = 1e-6, atol = 1e-8)
+        Sm = S(t / tf)
+        SΛ = Sm * Λ
+        cache .-= iden ⊗ SΛ + conj(SΛ) ⊗ iden - transpose(Sm) ⊗ Λ - conj(Λ) ⊗ Sm
+    end
 end
 
 
