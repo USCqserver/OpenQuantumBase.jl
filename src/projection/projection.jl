5ba0bb36d2094778af697c35a0f9549089a24114
Huo Chen <neversakura@hotmail.com>
2020-09-21 11:28:27 -0700

    improve project_to_lowlevel function
    1. add forward and backward mode
    2. rotate degenerate subspace

--- src/projection/projection.jl
+++ src/projection/projection.jl
@@ -16,109 +16,26 @@ struct ProjectedSystem
     dθ::Array{Vector{Float64},1}
     """Projected system bath interaction operators"""
     op::Array{Array{Matrix{Float64},1},1}
+    """Direction for the calculation"""
+    direct::Symbol
+    """Number of leves to keep"""
+    lvl::Int
     """Energy eigenstates at the final time"""
     ref::Array{Float64,2}
-    """Number of levels being kept"""
-    lvl::Int
 end
 
-
-"""
-$(TYPEDEF)
-
-Object for a projected coupling parameterized by NIBA parameters. This object holds the numerical values at gridded points.
-
-# Fields
-
-$(FIELDS)
-"""
-struct ProjectedCoupling
-    """Time grid (unitless) for projection"""
-    s::Any
-    """``(σ_m - σ_n)^2``"""
-    a::Any
-    """``|σ_{mn}|^2``"""
-    b::Any
-    """``σ_{mn}(σ_m-σ_n)``"""
-    c::Any
-    """``σ_{mn}(σ_m+σ_n)``"""
-    d::Any
-end
-
-
-"""
-$(TYPEDEF)
-
-Object for a projected system Hamiltonian parameterized by NIBA parameters. This object holds the numerical values at gridded points.
-
-# Fields
-
-$(FIELDS)
-"""
-struct ProjectedTG
-    """Time grid (unitless) for projection"""
-    s::Any
-    """Frequencies of given basis states"""
-    ω::Any
-    """Adiabatic part"""
-    T::Any
-    """Geometric phase"""
-    G::Any
-end
-
-
-function ProjectedSystem(s, ref, lvl)
+function ProjectedSystem(s, lvl, direction, ref)
     len = length(s)
     ev = Vector{Vector{Float64}}()
     dθ = Vector{Vector{Float64}}()
     op = Vector{Vector{Matrix{Float64}}}()
-    ProjectedSystem(s, ev, dθ, op, ref, lvl)
+    ProjectedSystem(s, ev, dθ, op, direction, lvl, ref)
 end
-
-
-function ProjectedCoupling(s, lvl)
-    t_dim = length(s)
-    a = LinearIdxLowerTriangular(Float64, t_dim, lvl)
-    b = LinearIdxLowerTriangular(Float64, t_dim, lvl)
-    c = LinearIdxLowerTriangular(
-        Float64,
-        t_dim,
-        lvl;
-        idx_exchange_func=(x) -> -conj(x),
-    )
-    d = LinearIdxLowerTriangular(
-        Float64,
-        t_dim,
-        lvl;
-        idx_exchange_func=(x) -> conj(x),
-    )
-    ProjectedCoupling(s, a, b, c, d)
-end
-
-
-function ProjectedTG(s, lvl)
-    t_dim = length(s)
-    ω = Matrix{Float64}(undef, t_dim, lvl)
-    T = LinearIdxLowerTriangular(
-        Float64,
-        t_dim,
-        lvl;
-        idx_exchange_func=(x) -> x,
-    )
-    G = LinearIdxLowerTriangular(
-        Float64,
-        t_dim,
-        lvl;
-        idx_exchange_func=(x) -> -x,
-    )
-    ProjectedTG(s, ω, T, G)
-end
-
 
 """
 $(SIGNATURES)
 
-Project a Hamiltonian `H` to the lowest `lvl` level subspace. `s_axis` is the grid of (unitless) times on which the projection is calculated. `dH` is the derivative of the Hamiltonian. `coupling` is the system-bath interaction operator. Both of `coupling` and `dH` should be callable with annealing parameter `s`. `atol` and `rtol` are the absolute and relative error tolerance to distinguish two degenerate energy levels. Currently this function only support real Hamiltonian with non-degenerate energies.
+Project a Hamiltonian `H` to the lowest `lvl` level subspace. `s_axis` is the grid of (unitless) times on which the projection is calculated. `dH` is the derivative of the Hamiltonian. `coupling` is the system-bath interaction operator. Both of `coupling` and `dH` should be callable with annealing parameter `s`. `atol` and `rtol` are the absolute and relative error tolerance to distinguish two degenerate energy levels. `direction`, which can be either `:forward` or `backward`, controls whether to start the calcuation from the starting point or the end point. Currently this function only support real Hamiltonian with non-degenerate energies.
 """
 function project_to_lowlevel(
     H::AbstractHamiltonian{T},
@@ -128,29 +45,38 @@ function project_to_lowlevel(
     lvl=2,
     atol::Real=1e-6,
     rtol::Real=0,
+    direction=:forward,
     refs=zeros(0, 0),
 ) where {T <: Real,S <: Real}
 
+    if direction == :forward
+        _s_axis = s_axis
+        update_rule = push!
+    elseif direction == :backward
+        _s_axis = reverse(s_axis)
+        update_rule = pushfirst!
+    else
+        throw(ArgumentError("direction $direction is not supported."))
+    end
     if isempty(refs)
-        w, v = H.EIGS(H, s_axis[1], lvl)
+        w, v = H.EIGS(H, _s_axis[1], lvl)
         # this is needed for StaticArrays
         w = w[1:lvl]
         v = v[:, 1:lvl]
         d_inds = find_degenerate(w, atol=atol, rtol=rtol)
         if !isempty(d_inds)
-            @warn "Possible degenerate detected at" s_axis[1]
-            @warn "With levels" d_inds
+            @warn "Degenerate energy levels detected at" _s_axis[1]
+            @warn "With" d_inds
         end
         refs = Array(v)
-        rotate_refs!(refs, v, lvl)
-        projected_system = ProjectedSystem(s_axis, refs, lvl)
-        push_params!(projected_system, w, v, dH(s_axis[1]), coupling(s_axis[1]), d_inds)
-        s_axis = s_axis[2:end]
+        projected_system = ProjectedSystem(s_axis, lvl, direction, refs)
+        update_params!(projected_system, w, dH(_s_axis[1]), coupling(_s_axis[1]), update_rule, d_inds)
+        _s_axis = _s_axis[2:end]
     else
-        projected_system = ProjectedSystem(s_axis, refs, lvl)
+        projected_system = ProjectedSystem(s_axis, lvl, direction, refs)
     end
 
-    for s in s_axis
+    for s in _s_axis
         w, v = H.EIGS(H, s, lvl)
         # this is needed for StaticArrays
         w = w[1:lvl]
@@ -160,8 +86,8 @@ function project_to_lowlevel(
             @warn "Possible degenerate detected at" s
             @warn "With levels" d_inds
         end
-        rotate_refs!(projected_system, v, lvl)
-        push_params!(projected_system, w, v, dH(s), coupling(s), d_inds)
+        update_refs!(projected_system, v, lvl, d_inds)
+        update_params!(projected_system, w, dH(s), coupling(s), update_rule, d_inds)
     end
     projected_system
 end
@@ -174,16 +100,27 @@ function project_to_lowlevel(
     lvl=2,
     atol::Real=1e-6,
     rtol::Real=0,
+    direction=:forward,
     refs=zeros(0, 0),
 ) where {T <: Complex,S <: Real}
     @warn "The projection method only works with real Hamitonians. Convert the complex Hamiltonian to real one."
     H_real = convert(Real, H)
-    project_to_lowlevel(H_real, s_axis, coupling, dH, lvl=lvl, atol=atol, rtol=rtol, refs=refs)
+    project_to_lowlevel(H_real, s_axis, coupling, dH, lvl=lvl, atol=atol, rtol=rtol, direction=direction, refs=refs)
 end
 
-function rotate_refs!(refs, v, lvl)
+function update_refs!(refs, v, lvl, d_inds)
+    # update reference vectors for degenerate subspace
+    if !isempty(d_inds)
+        for inds in d_inds
+            v[:, inds] = (v[:, inds] / refs[:, inds]) * v[:, inds]
+        end
+        flat_d_inds = reduce(vcat, d_inds)
+    else
+        flat_d_inds = []
+    end
     # update reference vectors for non-degenerate states
-    for i = 1:lvl
+    for i in (k for k in 1:lvl if !(k in flat_d_inds))
+    #for i in 1:lvl
         if v[:, i]' * refs[:, i] < 0
             refs[:, i] = -v[:, i]
         else
@@ -192,12 +129,12 @@ function rotate_refs!(refs, v, lvl)
     end
 end
 
-rotate_refs!(sys::ProjectedSystem, v, lvl) = rotate_refs!(sys.ref, v, lvl)
+update_refs!(sys::ProjectedSystem, v, lvl, d_inds) = update_refs!(sys.ref, v, lvl, d_inds)
 
-function push_params!(sys::ProjectedSystem, w, v, dH, interaction, d_inds)
+function update_params!(sys::ProjectedSystem, w, dH, interaction, update_rule, d_inds)
     # update energies
     E = w / 2 / π
-    push!(sys.ev, E)
+    update_rule(sys.ev, E)
     # update dθ
     dθ = Vector{Float64}()
     for j = 1:sys.lvl
@@ -213,10 +150,10 @@ function push_params!(sys::ProjectedSystem, w, v, dH, interaction, d_inds)
             end
         end
     end
-    push!(sys.dθ, dθ)
+    update_rule(sys.dθ, dθ)
     # update projected interaction operators
     op = [sys.ref' * x * sys.ref for x in interaction]
-    push!(sys.op, op)
+    update_rule(sys.op, op)
 end
 
 """
@@ -250,126 +187,4 @@ function concatenate(args...)
     ref = args[end].ref
     lvl = args[end].lvl
     ProjectedSystem(s, ev, dθ, op, ref, lvl)
-end
-
-
-function construct_projected_coupling(sys)
-    proj_c = ProjectedCoupling(sys.s, sys.lvl)
-    for (t_idx, op) in enumerate(sys.op)
-        for j = 1:sys.lvl
-            for i = (j + 1):(sys.lvl - j + 1)
-                proj_c.a[t_idx, i, j] = sum((x) -> (x[i, i] - x[j, j])^2, op)
-                proj_c.b[t_idx, i, j] = sum((x) -> abs2(x[i, j]), op)
-                proj_c.c[t_idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] - x[j, j]), op)
-                proj_c.d[t_idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] + x[j, j]), op)
-            end
-        end
-    end
-    proj_c
-end
-
-
-function construct_projected_TG(sys)
-    proj_tg = ProjectedTG(sys.s, sys.lvl)
-    for (idx, dθ) in enumerate(sys.dθ)
-        proj_tg.G[idx, :] = dθ
-        proj_tg.ω[idx, :] = sys.ev[idx]
-    end
-    fill!(proj_tg.T.mat, 0.0)
-    proj_tg
-end
-
-
-macro unitary_landau_zener(θ)
-    return quote
-        local val = $(esc(θ))
-        [cos(val) -sin(val); sin(val) cos(val)]
-    end
-end
-
-
-function landau_zener_rotate_angle(sys::ProjectedSystem, rotation_point)
-    non_rotation_idx = 1:(rotation_point - 1)
-    rotation_idx = rotation_point:length(sys.s)
-    dθ_itp = construct_interpolations(
-        sys.s[rotation_idx],
-        -get_dθ(sys, 2, 1)[rotation_idx],
-        extrapolation="line",
-    )
-    θᴸ_2 = [
-        quadgk(dθ_itp, sys.s[rotation_point], s)[1]
-        for s in sys.s[rotation_idx]
-    ]
-    θᴸ_1 = zeros(rotation_point - 1)
-    θᴸ = vcat(θᴸ_1, θᴸ_2)
-end
-
-
-function landau_zener_rotate(sys::ProjectedSystem, rotation_point)
-    θ = landau_zener_rotate_angle(sys, rotation_point)
-    non_rotation_idx = 1:(rotation_point - 1)
-    rotation_idx = rotation_point:length(sys.s)
-    proj_tg = ProjectedTG(sys.s, sys.lvl)
-    proj_c = ProjectedCoupling(sys.s, sys.lvl)
-
-    for idx in non_rotation_idx
-        proj_tg.ω[idx, :] = sys.ev[idx]
-        proj_tg.T[idx, :] = 0.0
-        proj_tg.G[idx, :] = sys.dθ[idx]
-        op = sys.op[idx]
-        for j = 1:sys.lvl
-            for i = (j + 1):(sys.lvl - j + 1)
-                proj_c.a[idx, i, j] = sum((x) -> (x[i, i] - x[j, j])^2, op)
-                proj_c.b[idx, i, j] = sum((x) -> abs2(x[i, j]), op)
-                proj_c.c[idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] - x[j, j]), op)
-                proj_c.d[idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] + x[j, j]), op)
-            end
-        end
-    end
-
-    for idx in rotation_idx
-        U = @unitary_landau_zener(θ[idx])
-        H = Diagonal(sys.ev[idx])
-        H = U' * H * U
-        proj_tg.ω[idx, :] = diag(H)
-        proj_tg.G[idx, :] = 0.0
-        op = [U' * x * U for x in sys.op[idx]]
-        for j = 1:sys.lvl
-            for i = (j + 1):(sys.lvl - j + 1)
-                proj_c.a[idx, i, j] = sum((x) -> (x[i, i] - x[j, j])^2, op)
-                proj_c.b[idx, i, j] = sum((x) -> abs2(x[i, j]), op)
-                proj_c.c[idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] - x[j, j]), op)
-                proj_c.d[idx, i, j] =
-                    sum((x) -> x[i, j] * (x[i, i] + x[j, j]), op)
-                proj_tg.T[idx, i, j] = H[i, j]
-            end
-        end
-    end
-    proj_tg, proj_c
-end
-
-
-Base.summary(TG::ProjectedTG) = string(TYPE_COLOR, nameof(typeof(TG)), NO_COLOR)
-
-
-function Base.show(io::IO, TG::ProjectedTG)
-    print(io, summary(TG))
-    print(io, " with size: ")
-    show(io, (size(TG.ω, 2), size(TG.ω, 2)))
-end
-
-
-Base.summary(C::ProjectedCoupling) =
-    string(TYPE_COLOR, nameof(typeof(C)), NO_COLOR)
-
-
-function Base.show(io::IO, C::ProjectedCoupling)
-    print(io, summary(C))
-    print(io, " with size: ")
-    show(io, (C.a.lvl, C.a.lvl))
 end
--- test/projection.jl
+++ test/projection.jl
@@ -17,6 +17,18 @@ t_obj = project_to_lowlevel(H, [0.0, 0.5, 1.0], coupling, dH)
     [[-1.0 0.0; 0.0 1.0]],
 ] atol = 1e-6
 
+# An exact solvable example
+H = DenseHamiltonian([(s)->cos(π*s/2), (s)->sin(π*s/2)], [σx, σz])
+dH(s) = π*(-sin(π*s/2)*σx+cos(π*s/2)*σz)/2
+coupling = ConstantCouplings(["Z"])
+
+t_obj = project_to_lowlevel(H, 0:0.01:1, coupling, dH)
+@test all((x)->isapprox(x, [-1, 1]), t_obj.ev)
+@test all((x)->isapprox(x[1], π/4), t_obj.dθ)
+t_obj = project_to_lowlevel(H, 0:0.01:1, coupling, dH, direction=:backward)
+@test all((x)->isapprox(x, [-1, 1]), t_obj.ev)
+@test all((x)->isapprox(x[1], π/4), t_obj.dθ)
+
 H = SparseHamiltonian(
     [(s) -> 1 - s, (s) -> s],
     [-standard_driver(2, sp=true) / 2, (spσz ⊗ spσi - 0.1spσz ⊗ spσz) / 2],
