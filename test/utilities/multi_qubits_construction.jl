736ec60ed33d486ac3ac0ae4c669b287d277fd40
Huo Chen <neversakura@hotmail.com>
2021-03-23 22:49:26 -0700

    ✨add new dispatch for single_clause
    Can directly use matrices to construct the clause now

--- test/utilities/multi_qubits_construction.jl
+++ test/utilities/multi_qubits_construction.jl
@@ -4,6 +4,8 @@ using OpenQuantumBase, Test
 @test single_clause(["x"], [2], 0.5, 2) == 0.5σi⊗σx
 @test single_clause(["x"], [2], 0.5, 2, sp = true) == 0.5spσi⊗spσx
 @test single_clause(["z","z"], [2,3], -2, 4) == -2σi⊗σz⊗σz⊗σi
+@test single_clause([σ₊], [3], 0.1, 3) == 0.1σi⊗σi⊗σ₊
+@test single_clause([σ₊], [3], 0.1, 3, sp=true) == 0.1spσi⊗spσi⊗σ₊
 @test standard_driver(2) == σx⊗σi + σi⊗σx
 @test standard_driver(2, sp = true) == spσx⊗spσi + spσi⊗spσx
 @test collective_operator("z", 3) ≈ σz⊗σi⊗σi + σi⊗σz⊗σi + σi⊗σi⊗σz
