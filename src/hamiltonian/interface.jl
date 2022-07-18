f4f1ff9ff9b50682a79376a9731dd05a767eecbf
Huo Chen <neversakura@hotmail.com>
2022-07-18 11:58:34 -0700

    🎨move general Hamiltonian interface to interface.jl file

---
+++ src/hamiltonian/interface.jl
@@ -0,0 +1,37 @@
+function ConstantHamiltonian(mat::Matrix; unit=:h, static=true)
+    # use static array for size smaller than 100
+    # can be turned off by setting `static` to false
+    if static && size(mat, 1) <= 10
+        mat = SMatrix{size(mat, 1),size(mat, 2)}(mat)
+        return ConstantStaticDenseHamiltonian(mat, unit=unit)
+    end
+    ConstantDenseHamiltonian(mat, unit=unit)
+end
+
+ConstantHamiltonian(mat::Union{SMatrix,MMatrix}; unit=:h, static=true) = ConstantStaticDenseHamiltonian(mat, unit=unit)
+
+function ConstantHamiltonian(mat::SparseMatrixCSC; unit=:h, static=true)
+    mat = unit_scale(unit) * mat
+    ConstantSparseHamiltonian(mat, size(mat))
+end
+
+function Hamiltonian(f, mats; unit=:h, dimensionless_time=true, static=true)
+    @warn "Elements in `mats` have different types. Will attempt to promote them to a common type."
+    mats = promote(mats)
+    Hamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time, static=static)
+end
+
+function Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:Matrix}
+    hsize = size(mats[1])
+    # use static array for size smaller than 100
+    # can be turned off by setting `static` to false
+    if static && hsize[1] <= 10
+        mats = [SMatrix{hsize[1],hsize[2]}(unit_scale(unit) * m) for m in mats]
+        return StaticDenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
+    end
+    DenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
+end
+
+Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:Union{SMatrix,MMatrix}} = StaticDenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
+
+Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:SparseMatrixCSC} = SparseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
