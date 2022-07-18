c366cfa3fc8665a952fe53b928c824c26101b8e7
Huo Chen <neversakura@hotmail.com>
2022-07-18 11:59:33 -0700

    🎨refactor `hamiltonian_base.jl`

--- src/hamiltonian/hamiltonian_base.jl
+++ src/hamiltonian/hamiltonian_base.jl
@@ -7,7 +7,7 @@ Fallback to `H.(s)/2/π` for generic `AbstractHamiltonian` type.
 """
 evaluate(H::AbstractHamiltonian, s::Real) = H.(s) / 2 / π
 
-function (H::AbstractHamiltonian, p, s::Real) 
+function (H::AbstractHamiltonian, ::Any, s::Real)
     H.(s) / 2 / π
 end
 
@@ -17,6 +17,10 @@ isconstant(H)
 Check whether a Hamiltonian is constant.
 """
 isconstant(::AbstractHamiltonian) = false
+Base.eltype(::AbstractHamiltonian{T}) where {T} = T
+Base.size(H::AbstractHamiltonian) = H.size
+Base.size(H::AbstractHamiltonian, dim::T) where {T<:Integer} = H.size[dim]
+get_cache(H::AbstractHamiltonian) = H.u_cache
 
 """
 $(SIGNATURES)
@@ -149,41 +153,3 @@ function is_complex(f_list, m_list)
         typeof(f(0)) <: Complex
     end
 end
-
-function ConstantHamiltonian(mat::Matrix; unit=:h, static=true)
-    # use static array for size smaller than 100
-    # can be turned off by setting `static` to false
-    if static && size(mat, 1) <= 10
-        mat = SMatrix{size(mat, 1), size(mat, 2)}(mat)
-        return ConstantStaticDenseHamiltonian(mat, unit=unit)
-    end
-    ConstantDenseHamiltonian(mat, unit=unit)
-end
-
-ConstantHamiltonian(mat::Union{SMatrix,MMatrix}; unit=:h, static=true)=ConstantStaticDenseHamiltonian(mat, unit=unit)
-
-function ConstantHamiltonian(mat::SparseMatrixCSC; unit=:h, static=true)
-    mat = unit_scale(unit) * mat
-    ConstantSparseHamiltonian(mat, size(mat))
-end
-
-function Hamiltonian(f, mats; unit=:h, dimensionless_time=true, static=true)
-    @warn "Elements in `mats` have different types. Will attempt to promote them to a common type."
-    mats = promote(mats)
-    Hamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time, static= static)
-end
-
-function Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:Matrix}
-    hsize = size(mats[1])
-    # use static array for size smaller than 100
-    # can be turned off by setting `static` to false
-    if static && hsize[1] <= 10
-        mats = [SMatrix{hsize[1],hsize[2]}(unit_scale(unit) * m) for m in mats]
-        return StaticDenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
-    end
-    DenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
-end
-
-Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:Union{SMatrix, MMatrix}} = StaticDenseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
-
-Hamiltonian(f, mats::AbstractVector{T}; unit=:h, dimensionless_time=true, static=true) where {T<:SparseMatrixCSC} = SparseHamiltonian(f, mats, unit=unit, dimensionless_time=dimensionless_time)
