6c2529d8bccad27f6366eebc829547ddfd979af1
Huo Chen <neversakura@hotmail.com>
2022-07-18 18:34:25 -0700

    🐛fix density matrix in-place update for `CustomDenseHamiltonian`

--- src/hamiltonian/custom_hamiltonian.jl
+++ src/hamiltonian/custom_hamiltonian.jl
@@ -1,26 +1,50 @@
 """
 $(TYPEDEF)
 
-Defines a time dependent Hamiltonian object with custom function.
+Defines a time dependent dense Hamiltonian object with custom function.
 
 # Fields
 
 $(FIELDS)
 """
-struct CustomDenseHamiltonian{T<:Number,in_place} <: AbstractDenseHamiltonian{T}
+struct CustomDenseHamiltonian{T<:Number,dimensionless_time,in_place} <: AbstractHamiltonian{T}
     """Function for the Hamiltonian `H(s)`"""
     f::Any
-    """Internal cache"""
-    u_cache::Any
     """Size"""
-    size::Any
+    size::Tuple
 end
 
 
-function hamiltonian_from_function(func; in_place=false)
+function hamiltonian_from_function(func; in_place=false, dimensionless_time=true)
     hmat = func(0.0)
-    CustomDenseHamiltonian{eltype(hmat),in_place}(func, nothing, size(hmat))
+    CustomDenseHamiltonian{eltype(hmat),dimensionless_time,in_place}(func, size(hmat))
 end
 
 get_cache(H::CustomDenseHamiltonian) = zeros(eltype(H), size(H))
+
 (H::CustomDenseHamiltonian)(s) = H.f(s)
+
+function update_cache!(cache, H::CustomDenseHamiltonian{T,dt,false}, ::Any, s::Real) where {T,dt}
+    cache .= -1.0im * H(s)
+end
+
+function update_cache!(cache, H::CustomDenseHamiltonian{T,dt,true}, ::Any, s::Real) where {T,dt}
+    H.f(cache, s)
+end
+
+function update_vectorized_cache!(cache, H::CustomDenseHamiltonian, ::Any, s::Real)
+    hmat = H(s)
+    iden = one(hmat)
+    cache .= 1.0im * (transpose(hmat) ⊗ iden - iden ⊗ hmat)
+end
+
+function (h::CustomDenseHamiltonian{T,dt,false})(du, u::AbstractMatrix, ::Any, s::Real) where {T,dt}
+    fill!(du, 0.0 + 0.0im)
+    H = h(s)
+    gemm!('N', 'N', -1.0im, H, u, 1.0 + 0.0im, du)
+    gemm!('N', 'N', 1.0im, u, H, 1.0 + 0.0im, du)
+end
+
+function (h::CustomDenseHamiltonian{T,dt,true})(du, u::AbstractMatrix, p::Any, s::Real) where {T,dt}
+    H.f(du, u, p, s)
+end
--- test/hamiltonian/custom_hamiltonian.jl
+++ test/hamiltonian/custom_hamiltonian.jl
@@ -1,4 +1,5 @@
 using OpenQuantumBase, Test
+import LinearAlgebra: Diagonal
 
 f(s) = (1 - s) * σx + s * σz
 H = hamiltonian_from_function(f)
@@ -9,3 +10,11 @@ H = hamiltonian_from_function(f)
 cache = get_cache(H)
 update_cache!(cache, H, 2.0, 0.5)
 @test cache == -0.5im * (σx + σz)
+
+du = zeros(ComplexF64, 2, 2)
+ρ = PauliVec[1][1] * PauliVec[1][1]'
+H(du, ρ, nothing ,0.5)
+@test du ≈ -1.0im * (f(0.5) * ρ - ρ * f(0.5))
+
+w, v = haml_eigs(H, 0.5, 2)
+@test v' * Diagonal(w) * v ≈ H(0.5)
